b08089f9bf3f5769e955959bb2d03f9eda1a92f4
ritzdixit <ritzdixit@gmail.com>
2026-03-07 23:44:37 +0530

    update

--- typing_app_backend/index.js
+++ typing_app_backend/index.js
@@ -18,17 +18,17 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect("mongodb://127.0.0.1:27017/signupdb")
-    .then(() => console.log("✅ Local MongoDB Connected"))
-    .catch(err => console.error("❌ MongoDB connection error:", err));
+// mongoose.connect("mongodb://127.0.0.1:27017/signupdb")
+//     .then(() => console.log("✅ Local MongoDB Connected"))
+//     .catch(err => console.error("❌ MongoDB connection error:", err));
 
 // Connect to Database for cloud deployment
-// mongoose.connect(process.env.MONGO_URI, {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true,
-// })
-// .then(() => console.log('✅ MongoDB connected'))
-// .catch(err => console.error('❌ MongoDB connection error:', err));    
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+.then(() => console.log('✅ MongoDB connected'))
+.catch(err => console.error('❌ MongoDB connection error:', err));    
 
 // Routes
 app.use('/api', authRoutes);
