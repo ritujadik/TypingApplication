0349775456524c05163f52fea0419bb3b22adfb2
ritzdixit <ritzdixit@gmail.com>
2026-03-07 23:31:04 +0530

    update

--- typing_app_backend/config/db.js
+++ typing_app_backend/config/db.js
@@ -1,14 +1,16 @@
-const mongoose = require('mongoose');
-
+const mongoose = require("mongoose");
 
 const connectDB = async () => {
-    try {
-        const conn = await mongoose.connect(process.env.MONGO_URI);
-        console.log('MongoDB connected');
-    } catch (error) {
-        console.error('MongoDB connection failed:', error.message);
-        process.exit(1);
-    }
+  try {
+    await mongoose.connect(process.env.MONGO_URI, {
+      serverSelectionTimeoutMS: 5000
+    });
+
+    console.log("MongoDB connected successfully");
+  } catch (error) {
+    console.error("MongoDB connection failed:", error);
+    process.exit(1);
+  }
 };
 
 module.exports = connectDB;
